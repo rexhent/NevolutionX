f8945f23be1fe35ad9802b886974b91af32f7747
Lucas Eriksson <dracc@dracc.se>
2020-04-12 01:14:46 +0200

    pvTester: Add reception control logic

--- Includes/pvTester.cpp
+++ Includes/pvTester.cpp
@@ -8,8 +8,7 @@ int pvTester(void*) {
   int listenfd = 0, connfd = 0;
   struct sockaddr_in serv_addr; 
   
-  char buf[1025];
-  size_t ticks; 
+  unsigned char buf[1025];
   
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
   memset(&serv_addr, '0', sizeof(serv_addr));
@@ -25,10 +24,17 @@ int pvTester(void*) {
   
   while(1)
   {
-    int n, bytes_recvd = 0;
+    size_t n, bytes_recvd = 0;
     connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
     printf("New connection\n");
     while ((n = read(connfd, buf, sizeof(buf)-1)) > 0) {
+      for (size_t i = 0; i < n - 1; ++i) {
+        if (((buf[i] != (buf[i+1] - 1)) &&
+             buf[i] != 255) ||
+            (buf[i] == 255 && buf[i+1] != 0)) {
+          printf("Bad value %d, index %d, other value %d\n", buf[i], i, buf[i+1]);
+        }
+      }
       bytes_recvd += n;
     }
     printf("Recv'd %d MiB\n", bytes_recvd/(1024*1024));
